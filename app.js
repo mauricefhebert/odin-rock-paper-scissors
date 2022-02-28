d52a061d504ebea40e4c47b32424a56020f71e4f
maurice <filiatreaultmaurice@gmail.com>
2022-02-28 14:40:01 -0500

    computerPlay function was build

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function computerPlay() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  const computerChoice = ['rock', 'paper', 'scissors'];
+  return computerChoice[randomNumber];
+}
+
+function playRound(playerSelection, computerSelection) {}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
