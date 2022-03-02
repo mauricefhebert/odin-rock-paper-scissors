8839405712690f75683e0e12e2f7e5763fda7b0b
maurice <filiatreaultmaurice@gmail.com>
2022-03-02 00:42:35 -0500

    Finish console version

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 //? This is use to check the winner of the game instead of a if...else
 const winMap = { rock: 'scissors', scissors: 'paper', paper: 'rock' };
+
+//? Track user and ai score
+let playerScore = 0;
+let computerScore = 0;
+
+//? Generate a random choice for the computer player
 function computerPlay() {
   const randomNumber = Math.floor(Math.random() * 3);
   const computerChoice = ['rock', 'paper', 'scissors'];
@@ -10,12 +16,28 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log('The game is Tie');
-  } else if (winMap[playerSelection] === computerSelection) {
+    playerScore++;
+    computerScore++;
+  }
+  //? Check if the player index of the player selection is equal to a win combination
+  else if (winMap[playerSelection] === computerSelection) {
     console.log(`You win this round! ${playerSelection} beat ${computerSelection}`);
+    playerScore++;
   } else {
     console.log(`You lose this round! ${computerSelection} beat ${playerSelection}`);
+    computerScore++;
   }
 }
-const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(round) {
+  for (let i = 0; i < round; i++) {
+    const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  console.log('Player Score ' + playerScore);
+  console.log('Computer Score ' + computerScore);
+  playerScore > computerScore ? console.log('You win the game!') : console.log('You lose the game!');
+}
+
+game(5);
