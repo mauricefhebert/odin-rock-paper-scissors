626f930d6dd43bd2422a907219e11fe7f9dc0202
maurice <filiatreaultmaurice@gmail.com>
2022-03-01 22:51:00 -0500

    Add a win map to check for the winner

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 'use strict';
-
+//? This is use to check the winner of the game instead of a if...else
+const winMap = { rock: 'scissors', scissors: 'paper', paper: 'rock' };
 function computerPlay() {
   const randomNumber = Math.floor(Math.random() * 3);
   const computerChoice = ['rock', 'paper', 'scissors'];
   return computerChoice[randomNumber];
 }
 
-function playRound(playerSelection, computerSelection) {}
-
-const playerSelection = 'rock';
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log('The game is Tie');
+  } else if (winMap[playerSelection] === computerSelection) {
+    console.log(`You win this round! ${playerSelection} beat ${computerSelection}`);
+  } else {
+    console.log(`You lose this round! ${computerSelection} beat ${playerSelection}`);
+  }
+}
+const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
