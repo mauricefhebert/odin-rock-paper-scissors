17c469e6e6f77c1238aeb9060c25e320f4ba78af
maurice <filiatreaultmaurice@gmail.com>
2022-03-13 21:46:38 -0400

    add color and general layout

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game(round) {
-  for (let i = 0; i < round; i++) {
-    const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  console.log('Player Score ' + playerScore);
-  console.log('Computer Score ' + computerScore);
-  playerScore > computerScore ? console.log('You win the game!') : console.log('You lose the game!');
-}
+// function game(round) {
+//   for (let i = 0; i < round; i++) {
+//     const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
+//     const computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+//   console.log('Player Score ' + playerScore);
+//   console.log('Computer Score ' + computerScore);
+//   playerScore > computerScore ? console.log('You win the game!') : console.log('You lose the game!');
+// }
 
-game(5);
+// game(5);
